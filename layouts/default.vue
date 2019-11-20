c9a6b0f55cd997d9c08eacf9b95de6d8366948ae
Louis Sandoval <louis@sandoval.com>
2019-11-19 21:42:18 -0700

    Fix header styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,27 +2,24 @@
 	<div>
 		<header class="header">
       <div class="header__rays" />
-      <div class="header__lower">
-        <div class="header__lower-left">
-          <div class="header__mara">
-            Maricopa ARA
-          </div>
-          <div class="header__copafest">
-            CopaFest 2020
-          </div>
+      <img
+        class="header__az-star"
+        src="/imgs/arizona_star.svg"
+        alt="AZ Star"
+      />
+      <div class="header__upper">
+        <div class="header__upper-left">
+          Maricopa ARA
         </div>
-        <div class="header__lower-middle">
-          <img
-            class="header__az-star"
-            src="/imgs/arizona_star.svg"
-            alt="AZ Star"
-          />
+        <div class="header__upper-right">
+          Presents
         </div>
-        <div class="header__lower-right">
-          <div class="header__copafest-date">
-            Saturday, October 24th, 2020
-          </div>
-        </div>
+      </div>
+      <div class="header__main-text">
+        CopaFest 2020
+      </div>
+      <div class="header__sub-text">
+        Saturday, October 24th, 2020
       </div>
 		</header>
 		<nuxt />
@@ -38,7 +35,7 @@
 
 body {
 	color: black;
-	background: linear-gradient(to bottom, $az-blue 25%, white);
+	background: white;
 	font-family: "Istok Web", sans-serif;
 }
 </style>
@@ -49,51 +46,61 @@ body {
 .header {
 	text-align: center;
 	font-family: "Lalezar", sans-serif;
-}
+  background-color: $az-blue;
 
-.header__rays {
-	background-image: url("/imgs/arizona_rays.svg");
-	background-size: cover;
-	background-position: bottom;
-	height: 30vh;
-}
+	&__rays {
+		background-image: url("/imgs/arizona_rays.svg");
+		background-size: cover;
+		background-position: bottom;
+		height: 30vh;
+	}
 
-.header__lower {
-	display: flex;
-	justify-content: center;
-	color: white;
-	text-shadow: -0.1rem -0.1rem 0 black, 0.1rem -0.1rem 0 black,
-		-0.1rem 0.1rem 0 black, 0.1rem 0.1rem 0 black;
-	background-color: $az-blue;
-}
+  @mixin vertical-adjustment {
+    transform: translateY(-80px);
+  }
 
-.header__lower-left {
-	text-align: right;
-}
+	&__az-star {
+		width: 10rem;
+		height: 10rem;
+    @include vertical-adjustment;
+	}
 
-.header__lower-middle {
-	text-align: center;
-}
+	@mixin header-text-properties {
+		color: white;
+		text-shadow: -0.1rem -0.1rem 0 black, 0.1rem -0.1rem 0 black,
+			-0.1rem 0.1rem 0 black, 0.1rem 0.1rem 0 black;
+		background-color: $az-blue;
+	}
 
-.header__az-star {
-	width: 15rem;
-	height: 15rem;
-	transform: translateY(-100px);
-}
+	&__upper {
+		display: flex;
+    align-items: baseline;
+		justify-content: space-around;
+		@include header-text-properties;
+    @include vertical-adjustment;
+  }
 
-.header__lower-right {
-	text-align: left;
-}
+	&__upper-left {
+		font-size: 1.5rem;
+	}
 
-.header__mara {
-	font-size: 1.5rem;
-}
+	&__upper-right {
+		font-size: 1.5rem;
+	}
 
-.header__copafest {
-	font-size: 4rem;
-}
+	&__main-text {
+		font-size: 4rem;
+		color: white;
+		@include header-text-properties;
+    @include vertical-adjustment;
+	}
 
-.header__copafest-date {
-	font-size: 2rem;
+  &__sub-text {
+    font-size: 2.3rem;
+    color: white;
+    @include header-text-properties;
+    @include vertical-adjustment;
+  }
+
 }
 </style>
