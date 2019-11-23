128fecfae74bd0f745f8ab536683d42445c09761
Louis Sandoval <louis@sandoval.com>
2019-11-23 00:09:01 -0700

    Center the footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,36 +1,41 @@
 <template>
-	<footer class="footer">
-		<div class="columns">
-			<div class="column">
-				<p>
-					Please consider donating, it'll fund future CopaFests and
-					other club activities to keep the hobby of ham radio alive.
-				</p>
-			</div>
-			<div class="column">
-				<form
-					action="https://www.paypal.com/cgi-bin/webscr"
-					method="post"
-					target="_blank"
-				>
-					<input type="hidden" name="cmd" value="_s-xclick" />
-					<input
-						type="hidden"
-						name="hosted_button_id"
-						value="AL3TRQJ78JY6E"
-					/>
-					<button
-						type="submit"
-						class="square-button square-button--yellow"
-					>
-						Donate
-					</button>
-				</form>
-			</div>
-		</div>
+	<footer class="footer has-text-centered">
 		<div class="content">
-			Maricopa Amateur Radio Association is a 501(c)(3) non-profit
-			corporation.
+			<div class="columns">
+				<div class="column">
+					<p>
+						Please consider donating, it'll fund future CopaFests
+						and other club activities to keep the hobby of ham radio
+						alive.
+					</p>
+				</div>
+				<div class="column">
+					<form
+						action="https://www.paypal.com/cgi-bin/webscr"
+						method="post"
+						target="_blank"
+					>
+						<input type="hidden" name="cmd" value="_s-xclick" />
+						<input
+							type="hidden"
+							name="hosted_button_id"
+							value="AL3TRQJ78JY6E"
+						/>
+						<button
+							type="submit"
+							class="square-button square-button--yellow"
+						>
+							Donate
+						</button>
+					</form>
+				</div>
+			</div>
+			<div class="columns">
+				<div class="column">
+					Maricopa Amateur Radio Association is a 501(c)(3) non-profit
+					corporation.
+				</div>
+			</div>
 		</div>
 	</footer>
 </template>
