be7df40332716f99e297e3b1363b7ab836543d5c
Louis Sandoval <louis@sandoval.com>
2019-11-23 00:03:14 -0700

    Fix overflow in footer

---
+++ components/footer.vue
@@ -0,0 +1,44 @@
+<template>
+	<footer class="footer">
+		<div class="columns">
+			<div class="column">
+				<p>
+					Please consider donating, it'll fund future CopaFests and
+					other club activities to keep the hobby of ham radio alive.
+				</p>
+			</div>
+			<div class="column">
+				<form
+					action="https://www.paypal.com/cgi-bin/webscr"
+					method="post"
+					target="_blank"
+				>
+					<input type="hidden" name="cmd" value="_s-xclick" />
+					<input
+						type="hidden"
+						name="hosted_button_id"
+						value="AL3TRQJ78JY6E"
+					/>
+					<button
+						type="submit"
+						class="square-button square-button--yellow"
+					>
+						Donate
+					</button>
+				</form>
+			</div>
+		</div>
+		<div class="content">
+			Maricopa Amateur Radio Association is a 501(c)(3) non-profit
+			corporation.
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: "footer"
+};
+</script>
+
+<style scoped lang="scss"></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,51 +10,21 @@
 			/>
 		</header>
 		<nuxt />
-		<footer class="footer">
-			<div class="level">
-				<div class="level-item">
-					<p>
-						Please consider donating, it'll fund
-						future CopaFests and other club activities to keep the
-						hobby of ham radio alive.
-					</p>
-					<form
-						action="https://www.paypal.com/cgi-bin/webscr"
-						method="post"
-						target="_blank"
-					>
-						<input type="hidden" name="cmd" value="_s-xclick" />
-						<input
-							type="hidden"
-							name="hosted_button_id"
-							value="AL3TRQJ78JY6E"
-						/>
-						<button
-							type="submit"
-							class="square-button square-button--yellow"
-						>
-							Donate
-						</button>
-					</form>
-				</div>
-			</div>
-			<div class="content">
-				Maricopa Amateur Radio Association is a 501(c)(3) non-profit
-				corporation.
-			</div>
-		</footer>
+		<copa-footer />
 	</div>
 </template>
 
 <script>
 	import navbar from "../components/navbar";
+	import footer from "../components/footer";
 
 	export default {
 	mounted() {
 		document.documentElement.classList.add("has-navbar-fixed-top");
 	},
 	components: {
-		navbar
+		navbar,
+		"copa-footer": footer
 	}
 };
 </script>
