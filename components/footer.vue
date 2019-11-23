bd8cf3a7f1603408fce7dfda58552b09f64a04f0
Louis Sandoval <louis@sandoval.com>
2019-11-23 14:51:39 -0700

    Reformat footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,40 +1,48 @@
 <template>
 	<footer class="footer has-text-centered">
-		<div class="content">
-			<div class="columns">
-				<div class="column">
+		<div class="columns">
+			<div class="column">
+				<p>
+					Please consider donating, it'll fund future CopaFests and
+					other club activities to keep the hobby of ham radio alive.
+				</p>
+				<form
+					action="https://www.paypal.com/cgi-bin/webscr"
+					method="post"
+					target="_blank"
+				>
+					<input type="hidden" name="cmd" value="_s-xclick" />
+					<input
+						type="hidden"
+						name="hosted_button_id"
+						value="AL3TRQJ78JY6E"
+					/>
+					<button
+						type="submit"
+						class="square-button square-button--yellow"
+					>
+						Donate
+					</button>
+				</form>
+			</div>
+		</div>
+		<div class="columns has-text-centered">
+			<div class="column">
+				<div class="content">
 					<p>
-						Please consider donating, it'll fund future CopaFests
-						and other club activities to keep the hobby of ham radio
-						alive.
+						Maricopa Amateur Radio Association is a 501(c)(3)
+						non-profit corporation.
 					</p>
 				</div>
-				<div class="column">
-					<form
-						action="https://www.paypal.com/cgi-bin/webscr"
-						method="post"
-						target="_blank"
-					>
-						<input type="hidden" name="cmd" value="_s-xclick" />
-						<input
-							type="hidden"
-							name="hosted_button_id"
-							value="AL3TRQJ78JY6E"
-						/>
-						<button
-							type="submit"
-							class="square-button square-button--yellow"
-						>
-							Donate
-						</button>
-					</form>
-				</div>
-			</div>
-			<div class="columns">
-				<div class="column">
-					Maricopa Amateur Radio Association is a 501(c)(3) non-profit
-					corporation.
-				</div>
+				<a href="http://www.copahams.org/" target="_blank">
+					<img
+						class="mara-logo is-rounded"
+						src="imgs/mara_logo.svg"
+					/>
+					<div>
+						CopaHams.org
+					</div>
+				</a>
 			</div>
 		</div>
 	</footer>
@@ -46,4 +54,9 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.mara-logo {
+	width: 14rem;
+	height: 14rem;
+}
+</style>
