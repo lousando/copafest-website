6697e398dec76c66b17697f5e5e7e4e4cc2d2a1c
Louis Sandoval <louis@sandoval.com>
2019-12-10 20:26:49 -0700

    Make volunteer page configure-able.

--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -3,52 +3,16 @@
 		<div class="container is-fluid">
 			<div class="content">
 				<div class="box">
-					<h2>Volunteer Information:</h2>
-					<p>
-						We can't have enough help. XYLs and kids are welcome to
-						help out. We need help in these areas:
-					</p>
-
-					<ul>
-						<li>
-							<b>Gatekeepers:</b> We need four people, working in
-							two three-hour shifts of two people each, to take
-							the money and issue wristbands.
-						</li>
-						<li>
-							<b>Raffle Ticket Sellers:</b> We need at least four
-							people walking the crowd selling raffle tickets.
-							Raffle tickets have been our largest source of
-							income in the past and getting them sold means money
-							for the club. We have close to a thousand dollars of
-							expense to cover before we realize any profit.
-						</li>
-						<li>
-							<b>Booth Representatives:</b> We need another four
-							people to sit the booth, in shifts, pass out
-							information, answer questions, and sell raffle
-							tickets.
-						</li>
-						<li>
-							<b>Sign Makers:</b> If someone in your family has
-							some artistic talent and would like to make signs
-							for the various seminars, etc., let me know. We will
-							provide the materials.
-						</li>
-						<li>
-							<b>Setup and take down:</b> Setup with start at 4:30
-							AM when the tailgaters arrive and breakdown will
-							happen after 1:00 PM. Any number of hands to help
-							load the trailer and the truck will be a help.
-						</li>
-					</ul>
+					<h2 v-text="volunteerSettings.header" />
+					<div v-html="volunteerSettings.content" />
 					<div>
 						<a
 							href="mailto:fest@copafest.org?subject=I'd%20like%20to%20volunteer%20for%20CopaFest&body=Hey!%0A%0AI'd%20like%20to%20help%20with..."
 						>
-							<button class="square-button square-button--yellow">
-								Sign me up!
-							</button>
+							<button
+								class="square-button square-button--yellow"
+								v-text="volunteerSettings.button_text"
+							/>
 						</a>
 					</div>
 				</div>
@@ -58,8 +22,15 @@
 </template>
 
 <script>
-export default {
-	name: "volunteer"
+  import volunteerSettings from "../assets/settings/pages/volunteer";
+
+  export default {
+	name: "volunteer",
+	data() {
+		return {
+			volunteerSettings
+		};
+	}
 };
 </script>
 
