f028fa26b93e309f9e34046c0429aa5495506381
Louis Sandoval <louis@sandoval.com>
2020-05-10 00:40:49 -0700

    Set lang attribute for screen readers

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,9 @@ export default {
 	 ** Headers of the page
 	 */
 	head: {
+    htmlAttrs: {
+      lang: "en"
+    },
 		title: `CopaFest ${DateTime.fromISO(settings.copa_fest_date).year}`,
 		meta: [
 			{ charset: "utf-8" },
