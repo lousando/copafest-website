eb46d2a3889f1750b354740897b67f7b17c2fdc1
Louis Sandoval <louis@sandoval.family>
2024-01-20 15:32:17 -0700

    Configure to use Storyblok global settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,22 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
 import vue from '@astrojs/vue';
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+      apiOptions: {
+        region: 'eu', // where our content is stored
+      },
+    }),
+    vue()
+  ],
 });
