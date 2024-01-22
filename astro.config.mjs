03c8f4d621bf2ce53fae91f48beeb9a60e1b3470
Louis Sandoval <louis@sandoval.family>
2024-01-21 21:13:29 -0700

    Migrate data to Storyblok CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import vue from "@astrojs/vue";
 import { loadEnv } from "vite";
 
-const env = loadEnv("", process.cwd(), 'STORYBLOK');
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,12 +11,26 @@ export default defineConfig({
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       components: {
-        // Add your components here
+        // layout
+        box: "storyblok/box",
+        row: "storyblok/row",
+        column: "storyblok/column",
+        paragraph: "storyblok/paragraph",
+        // etc
+        navbarItem: "storyblok/navbarItem",
+        sponsor: "storyblok/sponsor",
+        sponsorsContainer: "storyblok/sponsorsContainer",
+        infoBox: "storyblok/infoBox",
+        rafflePrize: "storyblok/rafflePrize",
+        rafflePrizeContainer: "storyblok/rafflePrizeContainer",
+        button: "storyblok/button",
+        tailgaterForm: "storyblok/tailgaterForm",
+        googleMap: "storyblok/googleMap"
       },
       apiOptions: {
-        region: 'eu', // where our content is stored
-      },
+        region: "eu" // where our content is stored
+      }
     }),
-    vue()
-  ],
+    vue(),
+  ]
 });
