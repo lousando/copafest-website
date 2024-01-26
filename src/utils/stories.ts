ce995952c9f861140c1510e924a4c3fb461567ab
Louis Sandoval <louis@sandoval.family>
2024-01-25 17:38:13 -0700

    Fix Typescript issues

--- src/utils/stories.ts
+++ src/utils/stories.ts
@@ -1,23 +1,29 @@
-import { useStoryblokApi } from "@storyblok/astro";
+import { type SbBlokData, useStoryblokApi } from "@storyblok/astro";
 
-export async function getAllStories() {
-  const storyblokApi = useStoryblokApi();
-  const storyBlockResponse = await storyblokApi.get(`cdn/stories`, {
-    version: import.meta.env.DEV ? "draft" : "published"
-  });
+export async function getAllStories(): Promise<Array<{
+	full_slug: string;
+	name: string;
+	content: {
+		page_content: Array<SbBlokData>
+	}
+}>> {
+	const storyblokApi = useStoryblokApi();
+	const storyBlockResponse = await storyblokApi.get(`cdn/stories`, {
+		version: import.meta.env.DEV ? "draft" : "published"
+	});
 
-  return storyBlockResponse.data.stories;
+	return storyBlockResponse.data.stories;
 }
 
 export async function getStory(slug = "") {
-  const storyblokApi = useStoryblokApi();
-  const storyBlockResponse = await storyblokApi.get(`cdn/stories/${slug}`, {
-    version: import.meta.env.DEV ? "draft" : "published"
-  });
+	const storyblokApi = useStoryblokApi();
+	const storyBlockResponse = await storyblokApi.get(`cdn/stories/${slug}`, {
+		version: import.meta.env.DEV ? "draft" : "published"
+	});
 
-  return storyBlockResponse.data.story.content;
+	return storyBlockResponse.data.story.content;
 }
 
 export async function getGlobalSettings() {
-  return getStory("global/settings");
+	return getStory("global/settings");
 }
